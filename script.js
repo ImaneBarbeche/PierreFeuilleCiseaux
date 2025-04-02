c7e8469af2382bb119b075351566cff3b0089a16
g-broche <gregory.broche@yahoo.fr>
2025-04-02 12:08:55 +0200

    refactored winning conditions and started to implement scoring system

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const gameOptions = ["pierre", "feuille", "ciseaux"]
 const formatter = new Intl.ListFormat("fr", { style: "long", type: "conjunction" });
-const winningTriad = {
-    pierre: "ciseaux",
-    feuille: "pierre",
-    ciseaux: "feuille"
+
+const cases = {
+    pierre: {
+        pierre: 0,
+        feuille: -1,
+        ciseaux: 1,
+    },
+    feuille: {
+        pierre: 1,
+        feuille: 0,
+        ciseaux: -1,
+    },
+    ciseaux: {
+        pierre: -1,
+        feuille: 1,
+        ciseaux: 0,
+    }
+}
+
+const score = {
+    player:0,
+    computer:0,
+    draw:0,
 }
 
 const pickARandomOption = () => {
@@ -16,16 +35,30 @@ let userChoice;
 do {
     userChoice = prompt(`Veulliez choisir une option entre ${formatter.format(gameOptions)}`);
     if(gameOptions.includes(userChoice)){
-        isUserChoiceValid=true
+        isUserChoiceValid=true;
+    }else{
+        alert(`Seules les options ${formatter.format(gameOptions)} sont valides!`);
     }
 } while (!isUserChoiceValid);
 const computerChoice = pickARandomOption();
 alert(`votre adversaire a choisi ${computerChoice}`)
-if(userChoice === computerChoice){
-    alert(`égalité (joueur : ${userChoice} ; ordinateur : ${computerChoice})`);
-}else if(winningTriad[userChoice] === computerChoice){
-    alert(`joueur a gagné (joueur : ${userChoice} ; ordinateur : ${computerChoice})`);
-}else{
-    alert(`ordinateur a gagné (joueur : ${userChoice} ; ordinateur : ${computerChoice})`);
+const result = cases[userChoice][computerChoice];
+switch (result) {
+    case -1:
+        score.computer++
+        alert(`ordinateur a gagné (joueur : ${userChoice} ; ordinateur : ${computerChoice}) score(W:${score.player} ; L:${score.computer} ; D:${score.draw})`);
+        break;
+    case 0:
+        score.draw++
+        alert(`égalité (joueur : ${userChoice} ; ordinateur : ${computerChoice}) score(W:${score.player} ; L:${score.computer} ; D:${score.draw})`);
+        break;
+    case 1:
+        score.player++
+        alert(`joueur a gagné (joueur : ${userChoice} ; ordinateur : ${computerChoice}) score(W:${score.player} ; L:${score.computer} ; D:${score.draw})`);
+        break;
+
+    default:
+        alert(`ce cas ne devrait jamais être atteint, appelez les devs!`)
+        break;
 }
 
